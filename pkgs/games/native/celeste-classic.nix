229adae5bbcb0a7595c47aa5756e0532af33bc80
mrtnvgr <root@unixis.fun>
2023-10-31 11:17:03 +0700

    pkgs(games/native/celeste-classic): fix

--- pkgs/games/native/celeste-classic.nix
+++ pkgs/games/native/celeste-classic.nix
@@ -1,8 +1,9 @@
 { stdenvNoCC, fetchzip, autoPatchelfHook, pkgs, lib, practiceMod ? false }:
 
 let
-  folder = if practiceMod then "CELESTE Practice Mod" else "CELESTE";
-  bin = if practiceMod then "celeste_practice_mod" else "celeste";
+  folder = if practiceMod then "CELESTE*Practice*" else "CELESTE";
+  srcbin = if practiceMod then "celeste_practice_mod" else "celeste";
+  outbin = if practiceMod then "celeste-classic-pm" else "celeste-classic";
 in stdenvNoCC.mkDerivation {
   pname = "celeste-classic";
   version = "1.0";
@@ -22,13 +23,13 @@ in stdenvNoCC.mkDerivation {
 
   installPhase = ''
     runHook preInstall
-    install -Dm755 */${folder}/${bin} $out/bin/celeste-classic
-    cp */${folder}/data.pod $out/bin/data.pod
+    install -Dsm755 */${folder}/${srcbin} $out/bin/${outbin}
+    install -Dm444 */${folder}/data.pod $out/bin/data.pod
     runHook postInstall
   '';
 
   meta = with lib; {
-    description = "A PICO-8 platformer about climbing a mountain, made in four days";
+    description = "A PICO-8 platformer about climbing a mountain, made in four days ${if practiceMod then "(Practice Mod)" else ""}";
     homepage = "https://celesteclassic.github.io/";
     license = licenses.unfree;
     platforms = platforms.linux;
