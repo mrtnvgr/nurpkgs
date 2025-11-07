915021b809f613e5a1d16b0709bf03cab6c4044f
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-07 21:01:13 +0700

    feat!(mkWineEnv, mkWineApp): revert "use concatScript", set allowSubstitutes

--- pkgs/wine-nixified/mkWineApp.nix
+++ pkgs/wine-nixified/mkWineApp.nix
@@ -13,37 +13,39 @@
 
 , meta ? { }
 
+, allowSubstitutes ? false
+
 , ...
 } @ envArgs:
 
 let
-  inherit (lib) optionalString concatScript;
+  inherit (lib) optionalString;
 
   env = mkWineEnv (envArgs // {
-    inherit name;
+    inherit name allowSubstitutes;
   });
-
-  app = writeShellApplication {
-    inherit name meta;
-
-    text = /* bash */ ''
-      . ${env}/bin/${name}
-
-      # $REPL is defined => start a shell in the env
-      if [ ! "$REPL" == "" ]; then
-        bash; exit 0
-      fi
-
-      ${optionalString (workdir != null) "cd \"${workdir}\""}
-
-      ${preScript}
-
-      ${optionalString isWindowsExe "wine ${wineFlags}"} "${executable}" "$@"
-
-      wineserver -w
-
-      ${postScript}
-    '';
-  };
 in
-concatScript name [ env app ]
+writeShellApplication {
+  inherit name meta allowSubstitutes;
+
+  runtimeInputs = [ env ];
+
+  text = /* bash */ ''
+    source ${name}
+
+    # $REPL is defined => start a shell in the env
+    if [ ! "$REPL" == "" ]; then
+      bash; exit 0
+    fi
+
+    ${optionalString (workdir != null) "cd \"${workdir}\""}
+
+    ${preScript}
+
+    ${optionalString isWindowsExe "wine ${wineFlags}"} "${executable}" "$@"
+
+    wineserver -w
+
+    ${postScript}
+  '';
+}
