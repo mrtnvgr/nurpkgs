aa32d074fb1c5e1daa31cac3dc0a2de71faa2a41
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-07 20:54:22 +0700

    mkWineApp: use concatScript

--- pkgs/wine-nixified/mkWineApp.nix
+++ pkgs/wine-nixified/mkWineApp.nix
@@ -17,31 +17,33 @@
 } @ envArgs:
 
 let
-  inherit (lib) optionalString;
+  inherit (lib) optionalString concatScript;
 
   env = mkWineEnv (envArgs // {
     inherit name;
   });
+
+  app = writeShellApplication {
+    inherit name meta;
+
+    text = /* bash */ ''
+      . ${env}/bin/${name}
+
+      # $REPL is defined => start a shell in the env
+      if [ ! "$REPL" == "" ]; then
+        bash; exit 0
+      fi
+
+      ${optionalString (workdir != null) "cd \"${workdir}\""}
+
+      ${preScript}
+
+      ${optionalString isWindowsExe "wine ${wineFlags}"} "${executable}" "$@"
+
+      wineserver -w
+
+      ${postScript}
+    '';
+  };
 in
-writeShellApplication {
-  inherit name meta;
-
-  text = /* bash */ ''
-    . ${env}/bin/${name}
-
-    # $REPL is defined => start a shell in the env
-    if [ ! "$REPL" == "" ]; then
-      bash; exit 0
-    fi
-
-    ${optionalString (workdir != null) "cd \"${workdir}\""}
-
-    ${preScript}
-
-    ${optionalString isWindowsExe "wine ${wineFlags}"} "${executable}" "$@"
-
-    wineserver -w
-
-    ${postScript}
-  '';
-}
+concatScript name [ env app ]
