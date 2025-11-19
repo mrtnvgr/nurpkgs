80cde108bf5aaa5ae249772bbdf53a6dd4fc42db
mrtnvgr <martynovegorOF@yandex.ru>
2025-11-19 19:35:44 +0700

    chore: remove unused logic

--- default.nix
+++ default.nix
@@ -1,20 +1,7 @@
-# This file describes your repository contents.
-# It should return a set of nix derivations
-# and optionally the special attributes `lib`, `modules` and `overlays`.
-# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
-# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
-# commands such as:
-#     nix-build -A mypackage
-
 { pkgs ? import <nixpkgs> { } }:
 let
   p = pkgs.callPackage;
 in rec {
-  # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
-
   # Soundfonts
   soundfont-touhou = p ./pkgs/soundfonts/touhou { };
 
